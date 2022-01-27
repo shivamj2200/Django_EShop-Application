f11d275e8c555990babac7334a2ef2c72347bc1e
shivamjaiswal2200@gmail.com <shivamjaiswal2200@gmail.com>
2022-01-27 15:50:49 +0530

    initial commit

---
+++ Assign1/polls/views/orders.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+
+from django.contrib.auth.hashers import check_password
+from polls.models.customer import Customer
+from django.views import View
+
+from polls.models.product import Product
+from polls.models.orders import Order
+from polls.middlewares.auth import auth_middleware
+
+class OrderView(View):
+  
+    def get(self , request ):
+        customer = request.session.get('customer')
+        orders = Order.get_orders_by_customer(customer)
+        print(orders)
+        return render(request , 'orders.html'  , {'orders' : orders})
+    
